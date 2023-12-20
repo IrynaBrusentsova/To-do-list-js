9dd5044bbc0888e322bf70b06791ab3961815b82
Iryna Brusentsova <bio200480@gmail.com>
2023-12-20 16:32:49 +0200

    Fix function

--- script.js
+++ script.js
@@ -6,59 +6,6 @@ console.log(button);
 console.log(input);
 console.log(list);
 
-// button.addEventListener("click", () => {
-//   console.log(input.value);
-//   // create elment li
-//   const li = document.createElement("li");
-//   // add class to li
-//   li.className = "todo-list-item";
-//   // create element button
-//   const deleteBtn = document.createElement("button");
-//   // add name to button
-//   deleteBtn.innerText = "Delete";
-//   // add class to button deleteDtn
-//   deleteBtn.className = "delete-button";
-
-//   // create text in li
-//   li.innerText = input.value;
-//   // add to list li with text
-//   list.appendChild(li);
-//   // add to li button
-//   li.appendChild(deleteBtn);
-//   input.value = "";
-
-//   //  delete li from list
-//   deleteBtn.addEventListener("click", () => {
-//     list.removeChild(li);
-//   });
-// });
-
-// enter from keybord
-
-// input.addEventListener("keydown", (e) => {
-//   if (e.keyCode === 13) {
-//     const li = document.createElement("li");
-//     // add class to li
-//     li.className = "todo-list-item";
-//     // create element button
-//     const deleteBtn = document.createElement("button");
-//     // add name to button
-//     deleteBtn.innerText = "Delete";
-//     // add class to button deleteDtn
-//     deleteBtn.className = "delete-button";
-//     // create text in li
-//     li.innerText = input.value;
-//     // add to list li with text
-//     list.appendChild(li);
-//     // add to li button
-//     li.appendChild(deleteBtn);
-//     input.value = "";
-//     deleteBtn.addEventListener("click", () => {
-//       list.removeChild(li);
-//     });
-//   }
-// });
-
 // ====
 function createAddElement() {
   const li = document.createElement("li");
@@ -83,11 +30,20 @@ function createAddElement() {
 }
 
 button.addEventListener("click", () => {
+  localStorage.setItem("task", input.value);
+  const li = document.createElement("li");
+  console.log(li);
+
+  li.textContent = localStorage.getItem("task");
+
   createAddElement();
 });
+
 // enter from keybord
 input.addEventListener("keydown", (e) => {
   if (e.keyCode === 13) {
     createAddElement();
   }
 });
+
+// localStorage.clear();
